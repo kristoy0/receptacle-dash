070454b0e4837e8bb0185627c303bd87480b1ce3
Kristo R <kristoy0@hotmail.com>
2017-11-22 22:02:18 +0200

    Add modals to start, stop, pause, unpause, delete functions & added details option

--- src/components/Containers.vue
+++ src/components/Containers.vue
@@ -11,7 +11,7 @@
             <th>Status</th>
             <th>Actions</th>
           </tr>
-          <tr v-for="item in items">
+          <tr v-for="item in items" :key="item.ID">
             <td>{{ item.ID.substring(0,12) }}</td>
             <td>{{ item.Name[0].substring(1) }}</td>
             <td>{{ item.Image }}</td>
@@ -19,26 +19,29 @@
             <td>{{ item.State }}</td>
             <td>
               <b-button-group>
-                <b-button variant="danger" size="sm" v-if="item.State == 'running'" v-on:click="stop(item.ID)"><i class="fa fa-stop-circle"></i></b-button>
-                <b-button variant="success" size="sm" v-else v-on:click="start(item.ID)"><i class="fa fa-play-circle"></i></b-button>
+                <b-btn size="sm" @click.stop="details(item, $event.target)">Details</b-btn>
+                <b-button variant="danger" size="sm" v-if="item.State == 'running'" v-on:click="action('stop',item.ID)"><i class="fa fa-stop-circle"></i></b-button>
+                <b-button variant="success" size="sm" v-else v-on:click="action('start', item.ID)"><i class="fa fa-play-circle"></i></b-button>
                 <b-dropdown size="sm" variant="primary" id="ddown1" text="">
-                  <b-dropdown-item v-if="item.State == 'paused'" v-on:click="unpause(item.ID)">Unpause</b-dropdown-item>
-                  <b-dropdown-item v-else v-on:click="pause(item.ID)">Pause</b-dropdown-item>
-                  <b-dropdown-item v-on:click="remove(item.ID)">Delete</b-dropdown-item>
+                  <b-dropdown-item v-if="item.State == 'paused'" v-on:click="action('unpause', item.ID)">Unpause</b-dropdown-item>
+                  <b-dropdown-item v-else v-on:click="action('pause', item.ID)">Pause</b-dropdown-item>
+                  <b-dropdown-item v-on:click="action('delete', item.ID)">Delete</b-dropdown-item>
                 </b-dropdown>
               </b-button-group>
             </td>
           </tr>
         </table>
       </div>
+      <b-modal id="modal1" @hide="resetModal" @ok="handleOk(modalDetails.action, modalDetails.id)">
+          <h4 class="my-1 py-1" slot="modal-header">Index: {{ modalDetails.header }}</h4>
+          <pre>{{ modalDetails.data }}</pre>
+      </b-modal>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
-
 const Url = 'http://localhost:8443/api'
-
 export default {
   name: 'Containers',
   components: {
@@ -46,7 +49,8 @@ export default {
   },
   data () {
     return {
-      items: []
+      items: [],
+      modalDetails: { index: '', data: '' }
     }
   },
   created () {
@@ -57,40 +61,30 @@ export default {
     })
   },
   methods: {
-    stop (id) {
-      this.$http.post(Url + '/containers/' + id + '/stop/').then(response => {
-        console.log('Stopped: ' + id)
+    details (item, button) {
+      this.modalDetails.header = item.ID.substring(0, 12)
+      this.modalDetails.data = JSON.stringify(item, null, 2)
+      this.$root.$emit('bv::show::modal', 'modal1', button)
+    },
+    resetModal () {
+      this.modalDetails.data = ''
+      this.modalDetails.header = ''
+      this.modalDetails.action = ''
+      this.modalDetails.id = ''
+    },
+    handleOk (action, id) {
+      this.$http.post(Url + '/containers/' + id + '/' + action + '/').then(response => {
+        console.log(action + ' ' + id)
       }, response => {
         console.log(response)
       })
     },
-    start (id) {
-      this.$http.post(Url + '/containers/' + id + '/start/').then(response => {
-        console.log('Started: ' + id)
-      }, response => {
-        console.log(response)
-      })
-    },
-    pause (id) {
-      this.$http.post(Url + '/containers/' + id + '/pause/').then(response => {
-        console.log('Paused: ' + id)
-      }, response => {
-        console.log(response)
-      })
-    },
-    unpause (id) {
-      this.$http.post(Url + '/containers/' + id + '/unpause/').then(response => {
-        console.log('Unpaused: ' + id)
-      }, response => {
-        console.log(response)
-      })
-    },
-    remove (id) {
-      this.$http.post(Url + '/containers/' + id + '/delete/true').then(response => {
-        console.log('Removed: ' + id)
-      }, response => {
-        console.log(response)
-      })
+    action (action, id, button) {
+      this.modalDetails.header = 'Confirm ' + action
+      this.modalDetails.data = 'Are you sure you want to ' + action + ' container: ' + id.substring(0, 12) + '?'
+      this.modalDetails.id = id
+      this.modalDetails.action = action
+      this.$root.$emit('bv::show::modal', 'modal1', button)
     }
   }
 }
