3d67ff6bb2a1a819e477b7371c7bac9e1b344cc6
Kristo R <kristoy0@hotmail.com>
2017-11-23 02:48:33 +0200

    Add host information to main page

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,12 +3,15 @@
     <Navbar/>
     <div class="container">
       <h1>Welcome to Receptacle!</h1>
+      <h5>Here are some host statistics</h5>
+      <pre>{{ hostStats }}</pre>
     </div>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
+const Url = 'http://localhost:8443/api'
 
 export default {
   name: 'Main',
@@ -17,7 +20,24 @@ export default {
   },
   data () {
     return {
+      hostStats: ''
     }
+  },
+  created () {
+    this.$http.get(Url).then(response => {
+      this.hostStats = response.body
+    }, response => {
+      console.log(response)
+    })
   }
 }
 </script>
+
+<style scoped>
+.container {
+  padding-top: 32px;
+}
+h5 {
+  padding-top: 32px;
+}
+</style>
